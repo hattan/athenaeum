b0219abe784a2fb218b825105cb53df15514f8f7
= <hattan@gmail.com>
2018-02-20 10:30:13 -0800

    refactored project to support client/server architecture

--- server/app/app.py
+++ server/app/app.py
@@ -6,10 +6,13 @@ blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint, doc='/documentation') #,doc=False
 
 app.register_blueprint(blueprint)
-
 app.config['SWAGGER_UI_JSONEDITOR'] = True
 
-a_session = api.model('Session', {'session' : fields.String('present this topic.')}) #, 'id' : fields.Integer('ID')
+a_session = api.model('Session', 
+    {
+        'title' : fields.String('present this topic.'),
+        'description' : fields.String('blah blah')
+    }) #, 'id' : fields.Integer('ID')
  
 sessions = []
 session = {'name' : 'Intro to Python', 'id' : 1}
@@ -17,13 +20,13 @@ sessions.append(session)
 
 @api.route('/sessions')
 class Session(Resource):
-
-    @api.marshal_with(a_session, envelope='the_data')
+    @api.marshal_with(a_session, envelope='data')
     def get(self):
         return sessions
 
     @api.expect(a_session)
     def post(self):
+        print(api.payload)
         new_session = api.payload 
         new_session['id'] = len(sessions) + 1
         sessions.append(new_session)
